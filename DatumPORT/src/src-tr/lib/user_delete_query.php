e16139e3e9377789262b693fc5dbd99f79b6ac03
Habil Yıldırım <135065802+YILDIRIMT@users.noreply.github.com>
2025-04-02 18:56:00 +0300

    Update user_delete_query.php

--- DatumPORT/src/src-tr/lib/user_delete_query.php
+++ DatumPORT/src/src-tr/lib/user_delete_query.php
@@ -1,18 +1,16 @@
 <?php
 include("DB_CONNECT.php");
 
-	$USERNAME_q = $_SESSION["USERNAME"];
-    $stmt = $connect->prepare("SELECT * FROM usrdta001 WHERE username_dataB = :username_dataB");
-    $stmt->execute(array(":username_dataB" => $USERNAME_q));
-    $USERNAMEECHO = $stmt->fetch();
+$USERNAME_q = $_SESSION["USERNAME"];
+$stmt = $connect->prepare("SELECT * FROM usrdta001 WHERE username_dataB = :username_dataB");
+$stmt->execute(array(":username_dataB" => $USERNAME_q));
+$USERNAMEECHO = $stmt->fetch();
 	
-	if($USERNAMEECHO > 0){
-		TRUE;
-	}
-	else {
-	 session_destroy();
-	 header("location: index.php");
-	 exit;
-	}
-
+if($USERNAMEECHO > 0){
+	TRUE;
+}else {
+	session_destroy();
+	header("location: index.php");
+	exit;
+}
 ?>
